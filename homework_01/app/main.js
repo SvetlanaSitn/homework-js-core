723de926840d393b19cef8d0f35bb17ccff59995
Svitlana Sytnikova <svitlana.sytnikovaa@gmail.com>
2020-07-01 06:06:02 +0300

    Added homework_01

--- homework_01/app/main.js
+++ homework_01/app/main.js
@@ -23,3 +23,49 @@ alert(`The area of a circle is ${Math.PI * radius ** 2}!`);
 let distanceToGet = prompt("Please, enter distance in km between two cities!", "");
 let hoursToGet = prompt("Please, enter expected time in hours!", "");
 alert(`Your necessary speed to get on time is about ${Math.round(distanceToGet / hoursToGet)} km/h.`); //? округление до ближайшего целого
+
+//* * 6.
+let usd = prompt("Please, enter the values of the USD!", "");
+const price = 0.89;
+alert(`${usd} USD = ${usd * price} EUR`);
+
+//* * 7.
+let flashDriveVolume = prompt("Please, enter your flash drive volume in GB!", "");
+let fileSize = 820;
+alert(`Your flash drive can fit ${Math.floor(flashDriveVolume * 1024 / fileSize)} files of size 820 MB!`);
+
+//* * 8.
+let usersCash = prompt("Please, enter your amount of money!", "");
+let chocoPrice = prompt("The price of one chocolate bar: ", "");
+let res = usersCash % chocoPrice;
+if (Math.floor(usersCash / chocoPrice) == 1) 
+{
+    res = res.toFixed(1);
+    alert(`You may buy only one bar of chocolate with these. And here's your change: ${res}.`);
+}
+else if (Math.floor(usersCash / chocoPrice) > 1)
+{
+    res = res.toFixed(1);
+    alert(`You may buy ${Math.floor(usersCash / chocoPrice)} bars of chocolate with these. And here's your change: ${res}.`);
+}
+else
+{
+    alert(`Not enough cash.`);
+}
+
+//* * 9.
+let userNum = +prompt("Please, enter three-digit number!", "");
+let resNum = 0;
+if (Number.isInteger(userNum) && userNum == 3) 
+{ 
+    while (userNum > 0) 
+    {
+        resNum = resNum * 10 + userNum % 10;
+        userNum = Math.floor(userNum / 10);
+    }
+    alert(`Number backwards is ${resNum}.`);
+}
+else
+{
+    alert(`It isn't a three-digit number!`);
+}
